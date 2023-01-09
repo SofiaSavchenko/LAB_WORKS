ac91fafe43882096c24ac3fa25e827775162a4d3
Sonya <savth2004@gmail.com>
2023-01-09 14:35:50 +0000

    initial_macro_command

---
+++ SpaceBattle.Lib/MacroCommand.cs
@@ -0,0 +1,18 @@
+namespace SpaceBattle.Lib;
+
+public class MacroCommand : ICommand
+{   
+    private List<ICommand> list_cmds;
+    public MacroCommand(List<ICommand> list_cmds)
+    {
+        this.list_cmds = list_cmds;
+    }
+    public void Execute()
+    {
+        foreach (var cmd in list_cmds)
+        {
+            cmd.Execute();
+        }
+    }
+}
+
